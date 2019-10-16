1c679ca875855c120fc6ebd0683be19795cf5f06
Luke Day <ld227707@falmouth.ac.uk>
2019-10-16 23:02:09 +0100

    Added basic player movement and initial UI.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1;
+
+    private Rigidbody2D rb;
+    private Vector3 dir;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        dir = new Vector3(x, y, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(dir * speed);
+    }
+}
